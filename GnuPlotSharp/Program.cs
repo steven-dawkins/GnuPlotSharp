bc7b83c717b41e8434557bbcc5738c6aa9860f92
Steven Dawkins <steven.dawkins@gmail.com>
2017-01-22 17:45:03 +0000

    initial invocation of gnuplot

--- GnuPlotSharp/Program.cs
+++ GnuPlotSharp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,37 @@ namespace GnuPlotSharp
     {
         static void Main(string[] args)
         {
+            var assemblyFile = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            var path = Path.Combine(assemblyFile.Directory.FullName, "gnuplot.exe");
+
+            var fi = new FileInfo(path);
+            if (!fi.Exists)
+            {
+                throw new Exception("Unable to locate gnuplot at: " + fi.FullName);
+            }
+
+            var scriptFile = @"c:\temp\plt.txt";
+            var dataFile = @"c:\temp\plt.dat";
+            var outputfile = "c:/temp/printme3.png";
+            var scriptContent = 
+$@"set term png
+set output ""{outputfile}""
+plot sin(x) * x - 10
+";
+
+            var data = @"
+0.1 5
+0.2 4
+0.3 3
+0.4 4
+";
+            File.WriteAllText(dataFile, data);
+
+            File.WriteAllText(scriptFile, scriptContent);
+
+            Process.Start(path, @"-c " + scriptFile);
+
+            Process.Start(outputfile);            
         }
     }
 }
