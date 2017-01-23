8a6a1350dd53194a258f4122d05dfcd3cc84a09c
Steven Dawkins <steven.dawkins@gmail.com>
2017-01-23 22:59:40 +0000

    refactored gnuplot launch logic into class

--- GnuPlotSharp/Program.cs
+++ GnuPlotSharp/Program.cs
@@ -9,12 +9,12 @@ using System.Threading.Tasks;
 
 namespace GnuPlotSharp
 {
-    public class Program
+    public class GnuPlotLauncher
     {
-        public static void Main(string[] args)
+        public void Launch(string arguments)
         {
             var assemblyFile = new FileInfo(Assembly.GetExecutingAssembly().Location);
-            var path = Path.Combine(assemblyFile.Directory.FullName, "gnuplot.exe");
+            var path = Path.Combine(assemblyFile.Directory.FullName, "gnuplot", "gnuplot.exe");
 
             var fi = new FileInfo(path);
             if (!fi.Exists)
@@ -22,29 +22,9 @@ namespace GnuPlotSharp
                 throw new Exception("Unable to locate gnuplot at: " + fi.FullName);
             }
 
-            var scriptFile = @"c:\temp\plt.txt";
-            var dataFile = @"c:/temp/plt.dat";
-            var outputfile = "c:/temp/printme3.png";
-            var scriptContent = 
-$@"set term png
-set output ""{outputfile}""
-plot [0.0:0.5] [2:6] ""{dataFile}"" with lines   title ""Hello World!!!""
-";
-
-            var data = @"
-0.1 5
-0.2 4
-0.3 3
-0.4 4
-";
-            Console.WriteLine(scriptContent);
-            File.WriteAllText(dataFile, data);
-
-            File.WriteAllText(scriptFile, scriptContent);
-
             var p = new Process();
             p.StartInfo.FileName = path;
-            p.StartInfo.Arguments = @"-c " + scriptFile;
+            p.StartInfo.Arguments = arguments;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.RedirectStandardError = true;
             p.StartInfo.UseShellExecute = false;
@@ -57,7 +37,36 @@ plot [0.0:0.5] [2:6] ""{dataFile}"" with lines   title ""Hello World!!!""
             {
                 throw new Exception("nonzero exit code");
             }
-            //Process.Start(outputfile);
+        }
+
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {            
+            var scriptFile = Path.GetTempFileName() + ".txt";
+            var dataFile = Path.GetTempFileName() + "plt.dat";
+            var outputfile = "c:/temp/printme4.png";
+            var scriptContent = 
+$@"set term png
+set output ""{outputfile}""
+plot [0.0:0.5] [2:6] ""{dataFile}"" with lines   title ""Hello World!!!""
+";
+
+            var data = @"
+0.1 5
+0.2 4
+0.3 3
+0.4 4
+";            
+            File.WriteAllText(dataFile, data);        
+            File.WriteAllText(scriptFile, scriptContent);
+
+            var arguments = @"-c " + scriptFile;
+            
+            Process.Start(outputfile);
 
             //Console.ReadLine();
         }
